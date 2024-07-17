e665b87f415a840cb129740ad2d4cb96b0cc9e20
Gopal Ji <112842172+igopalji@users.noreply.github.com>
2024-07-17 12:38:02 +0530

    scrt.js

--- scrt.js
+++ scrt.js
@@ -1,25 +1,3 @@
-
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Form validation
-    let name = document.getElementById('name').value.trim();
-    let email = document.getElementById('email').value.trim();
-    let message = document.getElementById('message').value.trim();
-
-    if (name === '' || email === '' || message === '') {
-        document.getElementById('form-message').innerText = 'All fields are required!';
-        return;
-    }
-
-    // Assuming you send the form data to a server here
-    // Example: sendFormData({ name, email, message });
-
-    document.getElementById('form-message').innerText = "Thank you for your message! We'll Contact you soon 😁"
-    document.getElementById('contact-form').reset();
-});
-
-
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -28,5 +6,3 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
-
-
